f80c0e95edfae8466c9171f83fec6edf1c041d86
caiser666 <chailirsentosa3696@gmail.com>
2021-10-09 12:29:03 +0700

    Update responsive text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-
+import React from "react";
 
 function App() {
   return (
     <div className="relative flex h-screen w-screen justify-center items-center">
-      <p className="text-7xl font-mono ">HELLO, REACT x TAILWIND + JIT</p>
+      <p className="font-mono text-center text-2xl m-8 sm:text-4xl sm:m-12 md:text-5xl md:m-24 lg:text-6xl lg:m-20 xl:text-7xl xl:md-16 transition-all duration-300 ease-in-out">
+        HELLO, REACT x TAILWIND + JIT
+      </p>
       <footer className="absolute bottom-0 mb-8">
-        <p className="text-gray-500 text-sm font-semibold">by caiser666 @ {(new Date().getFullYear())}</p>
+        <p className="text-gray-500 text-sm font-semibold">
+          by caiser666 @ {new Date().getFullYear()}
+        </p>
       </footer>
     </div>
   );
